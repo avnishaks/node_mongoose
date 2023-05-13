8ce7058cc69ec74a78a020d0ab34a07f679c06c9
Avnish Kumar <avnish.kumar@ACKO880.local>
2023-05-13 19:04:58 +0530

    Unique key validator key added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+var uniqueValidator = require('mongoose-unique-validator'); // unique validator
+
 
 const Schema = mongoose.Schema;
 mongoose.connect("mongodb://localhost:27017/university",{useNewUrlParser: true})
 
 const studentSchema = new Schema({
-    name: String,
+    name: {type:String, unique:true},
     age: Number,
     subject: String
 },
@@ -14,6 +16,9 @@ const studentSchema = new Schema({
     versionKey: false
 }
 )
+
+// set the plugin for unique key validation
+studentSchema.plugin(uniqueValidator);
 
 const Student = mongoose.model("Student",studentSchema);
 
@@ -24,6 +29,7 @@ const server=express()
 // Get API 
 server.get("/students",async(req,res)=>{
     const data= await Student.find({},{_id:0})
+    console.log(data);
     res.status(200).send({
         status: 'success',
         data:data
@@ -35,6 +41,7 @@ server.get("/students",async(req,res)=>{
 // POST API
 server.post("/students",async(req,res)=>{
     const data = new Student({name:req.query.name,age:req.query.age,subject:req.query.subject});
+    console.log(data);
     data.save().then((data) => {
         res.status(201).send(data);
     }).catch((error) => {
